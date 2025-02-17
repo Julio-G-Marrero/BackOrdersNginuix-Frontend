5c1eedd5febe093465a31b23824d4e03b5ce6199
Julio <julio.garcia135@hotmail.com>
2025-02-16 22:50:54 -0600

    backend fijo

--- src/services/axiosInstance.js
+++ src/services/axiosInstance.js
@@ -1,8 +1,9 @@
 import axios from "axios";
-const API_BASE_URL = import.meta.env.VITE_API_URL || "http://localhost:5000/api/v1"; // Para Vite
+// const API_BASE_URL = import.meta.env.VITE_API_URL || "http://localhost:5000/api/v1"; // Para Vite
 
 const axiosInstance = axios.create({
-  baseURL: API_BASE_URL,
+  // baseURL: API_BASE_URL,
+  baseURL: "https://backordersnginuix-backend-production.up.railway.app/api/v1",
   headers: {
     "Content-Type": "application/json",
   },
